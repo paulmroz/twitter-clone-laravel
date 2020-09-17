e00ff7b9a0116f8db54aa94ce869a08fcf8799fb
Paweł Mróz <paulmroz97@gmail.com>
2020-09-17 14:16:16 +0200

    Design the timeline

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,17 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+
+        <textarea name="body"  class="w-full" placeholder="Whats poppin?!"></textarea>
+
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+            <img src="https://picsum.photos/40/40" alt="" class="rounded-full mr-2">
+
+            <button class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white" type="submit">
+                Tweet this!
+            </button>
+        </footer>
+
+    </form>
+</div>
