c4e02ec3f955017eba37cabe0939ee13230f2b0f
Paweł Mróz <paulmroz97@gmail.com>
2020-09-18 17:21:27 +0200

    User can follow another user

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,13 +1,13 @@
-<div class="flex p-4 border-b border-gray-300">
+<div class="flex p-4 {{$loop->last ? '' : 'border-b border-gray-300'}} ">
     <div class="mr-2 flex-shrink-0">
-        <a href="{{route('profile', $tweet->user)}}">
+        <a href="{{ $tweet->user->path() }}">
             <img src="{{$tweet->user->avatar}}" alt="" class="rounded-full mr-2" width="50" height="50">
         </a>
     </div>
 
     <div>
         <h5 class="font-bold mb-4">
-            <a href="{{route('profile', $tweet->user)}}">
+            <a href="{{ $tweet->user->path() }}">
                 {{$tweet->user->name}}
             </a>
         </h5>
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,17 +1,23 @@
-@extends('components.app')
-
-@section('content')
+<x-app>
     <header class="mb-6 relative">
-        <img src="/images/default-banner.jpg" alt="" class="rounded-lg mb-2">
+        <div class="relative">
+            <img src="/images/default-banner.jpg" alt="" class="rounded-lg mb-2">
 
-        <div class="flex justify-between items-center mb-4">
+            <img src="{{$user->avatar}}" alt=""
+                 class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2"
+                 width="150"
+                 style="left:50%;"
+            >
+        </div>
+
+        <div class="flex justify-between items-center mb-6">
             <div>
                 <h2 class="font-bold text-2xl mb-0">{{$user->name}}</h2>
                 <p class="text-sm">Joined {{$user->created_at->diffForHumans() }}</p>
             </div>
-            <div>
+            <div class="flex">
                 <a href="" class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2">Edit profile</a>
-                <a href="" class="bg-blue-500 rounded-full py-2 px-4 text-white text-xs">Follow Me</a>
+                <x-follow-button :user="$user"></x-follow-button>
             </div>
         </div>
         <p class="text-sm">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
@@ -20,10 +26,7 @@
             qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur"
         </p>
 
-        <img src="{{$user->avatar}}" alt=""
-             class="rounded-full mr-2 absolute"
-             style="width: 150px; left: calc(50% - 75px); top:230px;"
-        >
+
 
 
     </header>
@@ -31,4 +34,4 @@
     @include('_timeline', [
     'tweets'=> $user->tweets
     ])
-@endsection
+</x-app>
